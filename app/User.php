0fa348d7c916710fc3baa13504df0573acffee43
Vadithalion <al375550@uji.es>
2020-06-04 12:47:37 +0200

    get all users

--- app/User.php
+++ app/User.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model
 {
-    //
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,11 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         User::truncate();
         Publication::truncate();
+        DB::table('follows')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
 
         factory(User::class, 10)->create();
         factory(Publication::class, 10)->create();
+
+        for($i=0;$i<10;$i++){
+        DB::table('follows')->insert([
+            'follower_id' => random_int(1,10),
+            'followed_id' => random_int(1,10),
+        ]);
+        }
+
     }
 }
